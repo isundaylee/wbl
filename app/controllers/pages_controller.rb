d209aaa8a0a70503447a5871d087104e7f33270e
Jiahao Li <isundaylee.reg@gmail.com>
2015-05-30 19:08:25 -0700

    Exec board showcase mockup.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,36 @@
 class PagesController < ApplicationController
   def homepage
+    @exec_board = [
+      {
+        name: 'Carolyn Chang',
+        position: 'Co-President'
+      },
+      {
+        name: 'Jenny  Lohmeier',
+        position: 'Co-President'
+      },
+      {
+        name: 'Tanya Talkar',
+        position: 'VP Internal Relations'
+      },
+      {
+        name: 'Angela Cui',
+        position: 'VP Corporate Relations'
+      },
+      {
+        name: 'Fiona Lam',
+        position: 'VP Finance'
+      },
+      {
+        name: 'Uma Girkar',
+        position: 'VP Operations'
+      },
+      {},
+      {
+        name: 'Sylvia Atsaves',
+        position: 'VP Marketing'
+      },
+      {}
+    ]
   end
 end
