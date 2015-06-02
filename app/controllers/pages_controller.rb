67db20f91229bdd4fd2e4bdf51668b4c2a46569b
Jiahao Li <isundaylee.reg@gmail.com>
2015-06-02 00:08:13 -0700

    Leadership page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,20 @@ class PagesController < ApplicationController
     end
   end
 
+  def leadership
+    @exec_board = Member.exec.to_a
+    @general_board = Member.all.to_a # For now
+
+    # Center the last one in case of 3n+1
+    if @exec_board.size % 3 == 1
+      @exec_board = @exec_board[0...-1] + [nil, @exec_board[-1], nil]
+    end
+
+    if @general_board.size % 3 == 1
+      @general_board = @general_board[0...-1] + [nil, @general_board[-1], nil]
+    end
+  end
+
   def posts
     @name = params[:name].gsub('-', '_')
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#homepage'
 
+  get 'leadership' => 'pages#leadership', as: 'leadership'
   get 'posts/:name' => 'pages#posts', as: 'posts'
 
   # Example of regular route:
