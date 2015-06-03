e7f8bca16ba578dd4a1c1148c64d9b8c51f28107
Jiahao Li <isundaylee.reg@gmail.com>
2015-06-03 00:34:38 -0700

    Customize rails_admin for post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,19 @@ class Post < ActiveRecord::Base
   before_save :normalize_name
 
   rails_admin do
-    field :name
-    field :content do
-      partial 'quill'
+    edit do
+      field :name
+      field :content do
+        partial 'quill'
+      end
+    end
+
+    list do
+      field :name
+    end
+
+    show do
+      field :name
     end
   end
 
